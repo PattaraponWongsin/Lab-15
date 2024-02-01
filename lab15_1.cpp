0767738b24def2ceb3a298b5bfb12ca52a71e916
pattarapon <honamiyue@gmail.com>
2024-02-01 09:45:18 +0700

    complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,27 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+    int a = 5;
+    char b = 'A';
+    char &c = b;
+    int *x = &a;
+    char *y = &b;
+    int **z= &x ;
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << " " << endl; //แสดงค่า 
+    cout << &a << " " << &b << " " << &c << " " << &x << " " << &y << " " << &z << " " << endl; //แสดง address
+    c = 'F';
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << " " << endl; //แสดงค่า 
+    *y = 'W';
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << " " << endl; //แสดงค่า 
+    *x = 6 ;
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << " " << endl; //แสดงค่า 
+    **z = 7 ;
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << " " << endl; //แสดงค่า 
+
 
 	
 	
 	return 0;
 }
+
